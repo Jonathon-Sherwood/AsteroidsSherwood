56fa94af9d9d9bf5c7e32a363679f0254fd06ba4
Jonathon Sherwood <jonathondsherwood@hotmail.com>
2020-06-03 12:19:17 -0700

    Player Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Transform tf;
+    public float turnSpeed = 1f; //Degrees per second.
+    public float moveSpeed = 5; //World Space Units per second.
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameManager.instance.player = this.gameObject;
+        tf = gameObject.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+        Shoot();
+    }
+
+    void Movement()
+    {
+        //Rotate player to the left
+        if (Input.GetKey(KeyCode.LeftArrow))
+            {
+            tf.Rotate(0, 0, turnSpeed * Time.deltaTime);
+            }
+        //Rotate player to the right
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            tf.Rotate(0, 0, -turnSpeed * Time.deltaTime);
+        }
+        //Move player forward
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            tf.position += tf.right * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //TODO: Implement shooting
+            Debug.Log("Shooting Not Implemented Yet");
+        }
     }
 }
