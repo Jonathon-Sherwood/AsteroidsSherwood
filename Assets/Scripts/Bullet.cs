ac280450afbfd48a37a12737fbaa988dd4a21a7a
Jonathon Sherwood <jonathondsherwood@hotmail.com>
2020-06-17 14:48:31 -0700

    Treasure Added

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -12,4 +12,19 @@ public class Bullet : MonoBehaviour
         //Moves based on player variables once instantiated by player Shoot function.
         transform.position += transform.right * bulletSpeed * Time.deltaTime;
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(this.gameObject);
+        Destroy(collision.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Gameplay Area"))
+        {
+            Destroy(this.gameObject);
+            Destroy(collision.gameObject);
+        }
+    }
 }
---
+++ Assets/Scripts/TreasureChest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureChest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!collision.gameObject.CompareTag("Gameplay Area"))
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                GameManager.instance.score += 500;
+            }
+
+            Destroy(this.gameObject);
+        }
+    }
+}
